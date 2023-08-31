d4c5022bdf65ec83bdd4ded69c7882ab69cb08ae
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-01 08:43:42 +0900

    finish the cards displaying with clicked tab

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ const loadData = async () => {
     );
     const mainData = await response.json();
     const data = mainData.data;
-    console.log(data);
+    // console.log(data);
 
     const tabContainer = document.getElementById("tab-container");
     data.forEach((x) => {
       // tab container
       const div = document.createElement("div");
       div.innerHTML = `
-      <a onclick="getData('${x.category_id}')" class="tab hover:bg-red-500">${x.category}</a>
+      <a onclick="getData('${x.category_id}')" class="tab hover:bg-red-500 hover:text-white rounded-sm">${x.category}</a>
       
       `;
       tabContainer.appendChild(div);
@@ -27,11 +27,46 @@ const getData = async (id) => {
     const response = await fetch(
       `https://openapi.programming-hero.com/api/videos/category/${id}`
     );
-    const data = await response.json();
+    const mainData = await response.json();
+    const data = mainData.data;
+
     console.log(data);
+
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = ""; // Clear previous cards
+    data.forEach((x) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+         <div class="card  bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="${x.thumbnail}"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <div class="avatar gap-4">
+                <div class="w-10  rounded-full">
+                  <img
+                    src="${x.authors[0].profile_picture}"
+                  />
+                </div>
+                <h2 class="card-title">${x.title}</h2>
+              </div>
+              <div class="ml-14">
+                <p>${x.authors[0].profile_name}</p>
+                <p>${x.others.views}</p>
+              </div>
+            </div>
+          </div>
+        `;
+      cardContainer.appendChild(div);
+    });
   } catch (error) {
     console.error("Error fetching categories:", error);
   }
 };
 
 loadData();
+// for default all displaying
+getData("1001");
